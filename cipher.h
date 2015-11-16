d3c51905ec7ac4d265e5ca22706214a103e5d5f6
kgoins <kgoins@github.com>
2015-11-15 19:08:00 -0500

    removed struct from header and test

--- cipher.h
+++ cipher.h
@@ -1,23 +1,13 @@
 #ifndef CIPHER_H
 #define CIPHER_H
 
-#include <stdlib.h>
-
 #define TEXT_MAX 1025
-
-/* global vars */
-typedef struct {
-    char* text;
-    int textLen;
-    int charKey, numKey;
-} cipher_t;
 
 int cipher(char* text, int lkey, int nkey);
 
-cipher_t* createCipher(char* msg, int lkey, int nkey);
-void destroyCipher(cipher_t* cipher);
+int processInput(char* text, int textMax);
 
-void sub(cipher_t* cipher);
-void trans(cipher_t* cipher);
+void sub(char* text, int lkey, int nkey, int textLength);
+void trans(char* text, int textLength);
 
 #endif
