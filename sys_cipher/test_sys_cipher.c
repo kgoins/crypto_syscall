1d3d5fc6fd3ae405abdffce66750557812a16fc2
Kgoins <kgoins@Box0.local>
2015-11-18 06:04:11 -0500

    finished syscalls and updated syscall test file

---
+++ sys_cipher/test_sys_cipher.c
@@ -0,0 +1,137 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* error codes */
+#define MALLOC_ERR 1
+#define TEST_FAIL 2
+#define LENGTH_MISMATCH 3
+
+#define CIPHER_TEXT_MAX 1025
+
+extern int cipher(char* text, int lkey, int nkey);
+
+int processInput(char* text) {
+    int textLength = 0;
+
+    /* calculate text length */
+    while(text[textLength] != '\0') {
+        ++textLength;
+    }
+    /* account for ending null terminator */
+    textLength += 1;
+
+    /* truncate string */
+    if(textLength > CIPHER_TEXT_MAX) {
+        textLength = CIPHER_TEXT_MAX;
+        text[textLength] = '\0';
+    }
+    
+    return textLength;
+}
+
+void run_test (char* msg, int lkey, int nkey) {
+    char* msgCopy = NULL;
+    int i = 0;
+    int msgOrigLen = 0, msgEncLen = 0, msgDecLen = 0;
+    int testErr = 0;
+
+    /* get length and sanitize msg */
+    msgOrigLen = processInput(msg);
+
+    /* copy sanitized msg to msgCopy */
+    msgCopy = (char*) malloc(msgOrigLen * sizeof(char));
+    if(msgCopy == NULL) {
+        printf("Malloc Error!\n");
+        return;
+    }
+
+    i = 0;
+    while(msg[i] != '\0') {
+        msgCopy[i] = msg[i];
+        ++i;
+    }
+    msgCopy[msgOrigLen] = '\0';
+    
+    /* run encrypt and decrypt */
+    printf("plaintext message: %s\n", msg);
+    printf("size: %d\n", msgOrigLen);
+    printf("\n");
+
+    printf("Enryption system errors:\n");
+    msgEncLen = cipher(msg, lkey, nkey);
+    printf("encrypted message: %s\n", msg);
+    printf("size: %d\n", msgEncLen);
+    printf("\n");
+
+    printf("Decryption system errors:\n");
+    msgDecLen = cipher(msg, -lkey, -nkey);
+    printf("decrypted message: %s\n", msg);
+    printf("size: %d\n", msgDecLen);
+
+    /* test message lengths to origional */
+    if (msgEncLen != msgOrigLen || msgDecLen != msgOrigLen) {
+        testErr = LENGTH_MISMATCH;
+        goto handle_errs;
+    }
+
+    /* compare decrypted and origional msg */
+    printf("Origional message: %s\n", msgCopy);
+
+    i = 0;
+    while(msg[i] != '\0') {
+        if (msg[i] != msgCopy[i]) {
+            testErr = TEST_FAIL;
+            break;
+        } else ++i;
+    }
+
+    /* handle errors */
+handle_errs:
+    switch(testErr) {
+        case 0:
+            printf("Test Passed!\n");
+            break;
+        case TEST_FAIL:
+            printf("Test Failed: Strings not equal\n");
+            break;
+        case LENGTH_MISMATCH:
+            printf("Test Failed: Length mismatch\n");
+            break;
+    }
+
+
+    printf("\n");
+    free(msgCopy);
+}
+
+int main (int argc, char const* argv[]) {
+    /* test keys */
+    int lkey = 'B';
+    int nkey = 'B';
+
+    /* test strings */
+    char subTest[] = "Hello World!";
+    char transTest_0[] = "Hell";
+    char transTest_1[] = "HHHello";
+    char truncationTest[1030];
+
+    /* run tests */
+    printf("Sub alg test on Hello World!\n");
+    run_test(subTest, lkey, nkey);
+
+    printf("Test trans alg with only one quad\n");
+    run_test(transTest_0, lkey, nkey);
+
+    printf("Trans alg test: 3 char last quad \n");
+    run_test(transTest_1, lkey, nkey);
+
+    printf("Creating huge string to test truncation\n");
+    int i;
+    for (i = 0; i < 1030; i++)
+        truncationTest[i] = 'x';
+    truncationTest[1030] = '\0';
+    run_test(truncationTest, lkey, nkey);
+
+
+    return 0;
+}
