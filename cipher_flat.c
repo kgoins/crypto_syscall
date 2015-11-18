cdb8361ae381c65b1d86af8f26b7342c26a06df3
Kgoins <kgoins@Box0.local>
2015-11-18 05:50:38 -0500

    created sys_cipher dir and finished sys_cipher implimentation

---
+++ cipher_flat.c
@@ -0,0 +1,109 @@
+#define CIPHER_TEXT_MAX 1025
+
+int cipher (char* text, int lkey, int nkey) {   
+    /* cipher root vars */
+    int textLength;
+
+    /* substitution alg vars */
+    int x, offset, lkeyHash, nkeyHash, decryptFlag;
+
+    /* transposition alg vars */
+    int lastQuad;
+
+    /* temp vars */
+    int i; char temp;
+
+    /**********************/
+    /* process input text */ 
+    /**********************/
+
+    textLength = 0;
+
+    /* calculate text length */
+    while(text[textLength] != '\0') {
+        ++textLength;
+    }
+    /* account for ending null terminator */
+    textLength += 1;
+
+    /* truncate string */
+    if(textLength > CIPHER_TEXT_MAX) {
+        textLength = CIPHER_TEXT_MAX;
+        text[textLength] = '\0';
+    }
+
+    /* textLength is now accurate and text is sanitized */
+    /* run encryption algorithms */
+
+    /*****************************/
+    /* run substitution on text */
+    /*****************************/
+    lkeyHash = (lkey % 26) + 26;
+    nkeyHash = (nkey % 10) + 10;
+
+    decryptFlag = ( (lkey < 0) && (lkey & 0x1) );
+
+    /* main substitution loop */
+    for (i = 0; i < textLength; i++) {
+        /* text[i] is upper case letter */
+        if(text[i] >= 'A' && text[i] <= 'Z') {
+            x = (text[i] - 'A' + lkeyHash) % 26;
+
+            if (decryptFlag)
+                offset = ((x-'A') & 0x1) ? 'A':'a';
+            else
+                offset = ((x-'A') & 0x1) ? 'a':'A';
+
+            text[i] = x + offset;
+        }
+        /* text[i] is lower case letter */
+        else if(text[i] >= 'a' && text[i] <= 'z') {
+            x = (text[i] - 'a' + lkeyHash) % 26;
+            offset = (decryptFlag) ? 
+                ( ( (x - 'A') & 0x1 ) ? 'a' : 'A' ) :
+                ( ( (x - 'A') & 0x1 ) ? 'A' : 'a') ;
+            text[i] = x + offset;
+        }
+        /* text[i] is digit */
+        else if(text[i] >= '0' && text[i] <= '9') {
+            text[i] = ((text[i] - '0' + nkeyHash) % 10) + '0';
+        }
+        else
+            continue;
+    } /***** END Substitution ******/
+    
+    /*****************************/
+    /* run transposition on text */
+    /*****************************/
+    lastQuad = textLength % 4;
+    /* process the text up to the last quad */
+    for (i = 0; i < (textLength - lastQuad); i+=4) {
+        temp = text[i];
+        text[i] = text[i+2];
+        text[i+2] = temp;
+
+        temp = text[i+1];
+        text[i+1] = text[i+3];
+        text[i+3] = temp;
+    } /* i holds the start of the last quad */
+
+    /* process the last quad if exists */
+    if (lastQuad !=0) 
+        switch(lastQuad) {
+            case 1:
+                break;
+
+            case 2:
+                temp = text[i];
+                text[i] = text[i+1];
+                text[i+1] = temp;
+                break;
+            case 3:
+                temp = text[i];
+                text[i] = text[i+2];
+                text[i+2] = temp;
+                break;
+        }/***** END Transposition ******/
+
+    return textLength;
+}
